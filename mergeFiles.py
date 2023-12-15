cfee6638ee23b9f1d98347afa3c6a8831b786781
kasunpavithra <mandalawattegekasunpavithra@gmail.com>
2023-12-16 02:12:24 +0530

    segment visualization with folium

--- mergeFiles.py
+++ mergeFiles.py
@@ -3,6 +3,6 @@ from utils.local.stack_files import stack_files
 if __name__ == '__main__':
     filesPath  = "./data/Raw-GPS-data-Kandy-Buses/MAIN/TEMP/AC_SF_CAL/10T/"
     savePath = './data/Raw-GPS-data-Kandy-Buses/MAIN/OUTPUTS/'
-    fileName = "merged_segements_10T.csv"
+    fileName = "merged_gps_data_10T.csv"
 
-    print(stack_files(filesPath,savePath,fileName,"segment"))
+    print(stack_files(filesPath,savePath,fileName,"gps"))
