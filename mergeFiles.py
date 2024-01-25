2f65b0cd8cee34c63a21ee2729ed53834da729ef
kasunpavithra <mandalawattegekasunpavithra@gmail.com>
2024-01-25 13:22:17 +0530

    minor changes: mergeFiles.py, and norms-method.ipynb

--- mergeFiles.py
+++ mergeFiles.py
@@ -1,8 +1,8 @@
 from utils.local.stack_files import stack_files
 
 if __name__ == '__main__':
-    filesPath  = "./data/Raw-GPS-data-Kandy-Buses/MAIN/TEMP/AC_SF_CAL/10T/"
+    filesPath  = "./data/Raw-GPS-data-Kandy-Buses/MAIN/TEMP/TR_EX/"
     savePath = './data/Raw-GPS-data-Kandy-Buses/MAIN/OUTPUTS/'
-    fileName = "merged_segements_10T.csv"
+    fileName = "merged_trips_data.csv"
 
-    print(stack_files(filesPath,savePath,fileName,"segment"))
+    print(stack_files(filesPath,savePath,fileName,"bus_trips"))
