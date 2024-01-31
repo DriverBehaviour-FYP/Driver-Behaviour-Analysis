6b6fdeb1e4336d90ff9f47ae447b2ed8f696b3b1
kasunpavithra <mandalawattegekasunpavithra@gmail.com>
2024-02-01 00:50:36 +0530

    feat(create-norms-cluster): completed clustering and norms

--- mergeFiles.py
+++ mergeFiles.py
@@ -1,7 +1,7 @@
 from utils.local.stack_files import stack_files
 
 if __name__ == '__main__':
-    filesPath  = "./data/Raw-GPS-data-Kandy-Buses/MAIN/TEMP/TR_EX/"
+    filesPath  = "./data/Raw-GPS-data-Kandy-Buses/MAIN/TEMP/AC_SF_CAL/10T/"
     savePath = './data/Raw-GPS-data-Kandy-Buses/MAIN/OUTPUTS/'
     fileName = "merged_gps_data_10T_with_maxs.csv"
 
